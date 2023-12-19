74d6691567cfdb4be4b5460d502221f8fd4b2982
AndersonSantosDias <andersonSD.dev@gmail.com>
2023-12-19 14:26:29 -0300

    add notes

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../Header/Header'
 import TaskEditor from '../TaskEditor/TaskEditor'
 import Task from '../Task/Task'
 import ModalViw from '../Modal/Modal'
+import './index.scss'
 
 
 function Home() {
@@ -29,13 +30,18 @@ function Home() {
   return (
     <>
     <Header/>
-    <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
-    <TaskEditor todoHandler={todoHandler} />
-    {todos.map( (todo, key) => (
-      <div key={todo.id} >
-        <Task todo={todo} deleteTodo={deleteTodo} modal={abrir}/>
+    <h1>Otimize seu tempo e se organize com o nosso planejador diario.</h1>
+    <section>
+      <div>
+        <TaskEditor todoHandler={todoHandler} />
+        {todos.map( (todo, key) => (
+          <div key={todo.id} >
+            <Task todo={todo} deleteTodo={deleteTodo} modal={abrir}/>
+          </div>
+        ) )}
       </div>
-    ) )}
+      <textarea name="" id="" cols="40" rows="13" placeholder='Area reservada para observações a respeito das tarefas, como horarios ou metodos de execução'></textarea>
+    </section>
     <ModalViw isOpen={openModal} modal={abrir} texto={textoModal} tarefa={editDelet} />
     </>
   )
